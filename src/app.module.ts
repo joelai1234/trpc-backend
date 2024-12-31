489a379130ef8966ed8ca7e814b11d5ab35bdf43
joe.lai <joelai1234567890@gmail.com>
2024-12-31 19:38:40 +0800

    feat: env

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,14 @@ import { RoomsModule } from './rooms/rooms.module';
 import { ChatModule } from './chat/chat.module';
 import { AiModule } from './ai/ai.module';
 
+console.log({
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+});
+
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -17,15 +25,19 @@ import { AiModule } from './ai/ai.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'nestjs_db',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      // database: process.env.DATABASE_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
       migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
       migrationsRun: true,
+      ssl: {
+        rejectUnauthorized: false, // 如果不验证证书
+      },
     }),
     UsersModule,
     AuthModule,
--- src/data-source.ts
+++ src/data-source.ts
@@ -5,11 +5,12 @@ config(); // Load .env file
 
 export default new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.DB_DATABASE || 'nestjs_db',
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: parseInt(process.env.DATABASE_PORT) || 5432,
+  username: process.env.DATABASE_NAME || 'postgres',
+  password: process.env.DATABASE_PASSWORD || 'postgres',
+  database: process.env.DATABASE_NAME || 'nestjs_db',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+  ssl: false,
 });
